dbcf650d2f2566ca18d5c55adc94cfa68e65f448
OLAJIDE BALOGUN <Balogunolajide2004@gmail.com>
2025-10-12 16:47:39 +0300

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -33,7 +33,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href=""
+                href="https://drive.google.com/file/d/1DF_JfFh-hPVMUFLmPzUrm-7p7dt6hY5K/view?usp=share_link"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
